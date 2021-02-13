fd74a253a8a9992cffd787c0d85259f4875fd7d4
catmandoo777 <catmandoo777@yandex.ru>
2021-02-14 01:43:00 +0300

    new changes

--- main.py
+++ main.py
@@ -42,6 +42,19 @@ def get_url(URL):
   else:
     return None  
 
+def pars1(url, data):
+
+  soup = BeautifulSoup(data, 'lxml')
+
+  name_product = soup.find('div', class_= 'page-header-wrapper').text.replace('\n', '').replace('  ', '')
+  articul = soup.find('div', class_= 'product-info-sky product-sky').text.replace('\n', '').replace('  ', '')
+  price = soup.find('div', class_='price js-product-price on-page product-info-price__price').text.replace('\n', '').replace('  ', '')
+  characteristics = soup.find('div', class_='characteristics-block characteristics-block__sky cell-12').text.replace('\n', '').replace('  ', '')
+  picters_url =soup.find('div', class_='gallery-main-wrapper text-center hide-sm').a.get('href')
+  other = soup.find('div', id = 'characteristics-block').text.replace('\n', '').replace('  ', '')
+
+  return {url:{'url': url, 'articul': articul, 'price': price, 'characteristics': characteristics, 'other': other, 'picters_url': picters_url }}
+
 def main():
   '''
   with open('url_catalog.txt',) as file:
@@ -64,7 +77,38 @@ def main():
       with open('check_list1.txt', 'a') as file:
         file.write(g + '\n')
         '''
+  with open('finish_data.json') as file:
+    data = json.load(file)
+
+  k = 0
+  for i, j in data.items():
+    print('Get_page: ' + i)
+    if len(j) < 2:
+      g = get_url(i)
+      if g != None:
+        try:
+          result = pars1(i, g)[i]
+          data[i] = result
+          with open('finish_data.json', 'w') as file:
+            json.dump(data, file, indent=4, ensure_ascii=False)
+        except Exception:
+          with open('error_pars_card.txt', 'a') as file:
+            file.write(f'Error: {i} \n')
+    else:
+      print('pass: ' + i)
+    k += 1
+    if k >= 5500:
+      break
+
+
+  '''
   
+  try:
+    print(pars1(URL, src))
+  except Exception:
+    with open('error_pars_card.txt', 'a') as file:
+      file.write(f'Error: {URL} \n')
+  '''
 
 '''
 with open('data.json') as file:
@@ -76,12 +120,8 @@ for i, j in data.items():
     with open('index2.html', 'w') as file:
       file.write(temp)
   break
-'''  
-primer = {'url':{'url': 'url', 'articul': 'articul', 'maider': 'maider', 'price': 'price', 'text': 'text', 'cheracters': 'cheracters', }}
+'''
 
-with open('index2.html') as file:
-  src = file.read()
 
-soup = BeautifulSoup(src, 'lxml')
-
-print(soup.find('div', class_= 'row product-main-wrapper'))
+if __name__ == '__main__':
+  main()
